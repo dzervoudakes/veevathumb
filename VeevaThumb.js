5c236a653a08618d6bb6788169bfc48879290551
Dan Zervoudakes <dan.zervoudakes@gmail.com>
2015-03-08 22:12:48 -0400

    Getting anal about spacing.

--- VeevaThumb.js
+++ VeevaThumb.js
@@ -22,10 +22,10 @@
 	
 	// Ask users which size they wish to export...
 	//
-	function askUser(){
+	function askUser() {
 		while ((newSize !== "1") || (newSize !== "2")){
-			newSize = prompt("Export: 1) 1024x768, or 2) 200x150?","");
-			if (newSize == "1" || newSize == "2"){
+			newSize = prompt("Export: 1) 1024x768, or 2) 200x150?", "");
+			if (newSize == "1" || newSize == "2") {
 				return newSize;
 			}
 		}
@@ -33,7 +33,7 @@
 	
 	// History rollback function...
 	//
-	function revertHistory(){
+	function revertHistory() {
 		var latestHistory = app.activeDocument.historyStates.length;
 		var revertTo = latestHistory - 2;
 		app.activeDocument.activeHistoryState = app.activeDocument.historyStates[revertTo];
@@ -42,17 +42,17 @@
 	
 	// The export function...
 	//
-	function exportThumbs(){
+	function exportThumbs() {
 		askUser(); // Obtain user input
-		for (var i = 0; i < openDocs.length; i++){ // Cycle through the documents
+		for (var i = 0; i < openDocs.length; i++) { // Cycle through the documents
 			app.activeDocument = openDocs[i];
-			if (newSize == "1"){
-				app.activeDocument.resizeImage(1024,768); // Typical thumbnail size for "Home" screens
-			} else if (newSize == "2"){
-				app.activeDocument.resizeImage(200,150); // Default thumbnail size for Veeva presentations
+			if (newSize == "1") {
+				app.activeDocument.resizeImage(1024, 768); // Typical thumbnail size for "Home" screens
+			} else if (newSize == "2") {
+				app.activeDocument.resizeImage(200, 150); // Default thumbnail size for Veeva presentations
 			}
 			var fullPath = app.activeDocument.path.fsName;
-			var Name = app.activeDocument.name.replace(/\.[^\.]+$/,"");
+			var Name = app.activeDocument.name.replace(/\.[^\.]+$/, "");
 			var saveFile = File(fullPath + "/" + Name + ".jpg");
 			app.activeDocument.exportDocument(saveFile, ExportType.SAVEFORWEB, exportJPG);
 			revertHistory(); // Undo the image resize after exporting
