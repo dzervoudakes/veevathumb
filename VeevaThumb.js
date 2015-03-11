f79a5d3ae6ee0b2080510cb7a499867f1e86eb96
Dan Zervoudakes <dan.zervoudakes@gmail.com>
2015-03-11 19:11:47 -0400

    Spaced out code a little for readability.

--- VeevaThumb.js
+++ VeevaThumb.js
@@ -23,8 +23,11 @@
 	// Ask users which size they wish to export...
 	//
 	function askUser() {
+		
 		while ((newSize !== "1") || (newSize !== "2")) {
+			
 			newSize = prompt("Export: 1) 1024x768, or 2) 200x150?", "");
+			
 			if (newSize == "1" || newSize == "2") {
 				return newSize;
 			}
@@ -34,8 +37,10 @@
 	// History rollback function...
 	//
 	function revertHistory() {
+		
 		var latestHistory = app.activeDocument.historyStates.length;
 		var revertTo = latestHistory - 2;
+		
 		app.activeDocument.activeHistoryState = app.activeDocument.historyStates[revertTo];
 		app.activeDocument.save();
 	};
@@ -43,17 +48,23 @@
 	// The export function...
 	//
 	function exportThumbs() {
+		
 		askUser(); // Obtain user input
+		
 		for (var i = 0; i < openDocs.length; i++) { // Cycle through the documents
+			
 			app.activeDocument = openDocs[i];
+			
 			if (newSize == "1") {
 				app.activeDocument.resizeImage(1024, 768); // Typical thumbnail size for "Home" screens
 			} else if (newSize == "2") {
 				app.activeDocument.resizeImage(200, 150); // Default thumbnail size for Veeva presentations
 			}
+			
 			var fullPath = app.activeDocument.path.fsName;
 			var Name = app.activeDocument.name.replace(/\.[^\.]+$/, "");
 			var saveFile = File(fullPath + "/" + Name + ".jpg");
+			
 			app.activeDocument.exportDocument(saveFile, ExportType.SAVEFORWEB, exportJPG);
 			revertHistory(); // Undo the image resize after exporting
 		}
